c49f1ffbca36d337ffe70c05e0e3a86d1b2cd96e
maruki00 <aitbaha48@gmail.com>
2025-09-29 20:08:12 +0100

    Update unpack.go

--- struct/unpack.go
+++ struct/unpack.go
@@ -8,6 +8,7 @@ import (
 )
 
 func Unpack(val string) (uint32, error) {
+	// pattern \xAB -> 4 symboles
 	if len(val)%4 != 0 {
 		return 0, errors.New("invalid data")
 	}
@@ -19,15 +20,8 @@ func Unpack(val string) (uint32, error) {
 	return v, nil
 }
 
-func Unpack2Hex(val string) (string, error) {
-	if len(val)%4 != 0 {
-		return "", errors.New("invalid data")
-	}
-	_bytesVal := []byte(val)
-	return hex.EncodeToString(_bytesVal), nil
-}
-
 func UnpackLong(val string) (uint64, error) {
+	// pattern \xAB -> 4 symboles
 	if len(val)%4 != 0 {
 		return 0, errors.New("invalid data")
 	}
@@ -37,4 +31,16 @@ func UnpackLong(val string) (uint64, error) {
 	binary.Decode(_bytesVal, binary.LittleEndian, &v)
 	fmt.Println(v, _bytesVal)
 	return v, nil
+}
+
+func Unpack2Hex(val string) (string, error) {
+	// pattern \xAB -> 4 symboles
+	if len(val)%4 != 0 {
+		return "", errors.New("invalid data")
+	}
+	_bytesVal := []byte(val)
+	dd := make([]byte, 8)
+	binary.Decode(_bytesVal, binary.BigEndian, dd)
+
+	return "0x" + hex.EncodeToString(dd), nil
 }
