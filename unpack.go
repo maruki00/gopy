e514b5f1d038d178d049b9dd4ac92c1ac55ebfdf
maruki00 <aitbaha48@gmail.com>
2025-09-29 19:32:25 +0100

    Update unpack.go

--- unpack.go
+++ unpack.go
@@ -7,9 +7,25 @@ import (
 	"fmt"
 )
 
-// func UnpackLong(val string) (int64, error) {
-// 	return 0, nil
-// }
+func Unpack(val string) (uint32, error) {
+	if len(val)%4 != 0 {
+		return 0, errors.New("invalid data")
+	}
+	_bytesVal := []byte(val)
+
+	var v uint32
+	binary.Decode(_bytesVal, binary.LittleEndian, &v)
+	fmt.Println(v, _bytesVal)
+	return v, nil
+}
+
+func Unpack2Hex(val string) (string, error) {
+	if len(val)%4 != 0 {
+		return "", errors.New("invalid data")
+	}
+	_bytesVal := []byte(val)
+	return hex.EncodeToString(_bytesVal), nil
+}
 
 func Unpack(val string) (uint32, error) {
 	if len(val)%4 != 0 {
