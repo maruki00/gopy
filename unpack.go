5982952726dda55bba3a4f9f2a29822b25e50567
maruki00 <aitbaha48@gmail.com>
2025-09-29 18:58:07 +0100

    Update unpack.go

--- unpack.go
+++ unpack.go
@@ -1,6 +1,20 @@
 package gopy
 
-func Unpack() (int64, error) {
+import (
+	"encoding/binary"
+	"fmt"
+)
 
+func UnpackLong(_type string, val int64) (int64, error) {
+	d := make([]byte, 8)
+	binary.Encode(d, binary.LittleEndian, val)
+	fmt.Println(d)
+	return 0, nil
+}
+
+func Unpack(val int32) (int64, error) {
+	d := make([]byte, 4)
+	binary.Encode(d, binary.LittleEndian, val)
+	fmt.Println(d)
 	return 0, nil
 }
