b2c029e0c54e581adb3bca59e376efca275be20b
maruki00 <aitbaha48@gmail.com>
2025-09-29 19:08:28 +0100

    Update unpack.go

--- unpack.go
+++ unpack.go
@@ -20,8 +20,8 @@ func Unpack(val int32) (int64, error) {
 	var out strings.Builder
 	dBytes := make([]byte, 4)
 	binary.Encode(dBytes, binary.LittleEndian, val)
-	for _, j := range dBytes {
-		out.WriteString(fmt.Sprintf("\\x%.2x", j))
+	for _, byte := range dBytes {
+		out.WriteString(fmt.Sprintf("\\x%.2x", byte))
 	}
 	fmt.Println(out.String())
 	return 0, nil
